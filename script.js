ee39831099ee5e7bc9befb7960070121e4099c46
Ganesh-Patankar <ganeshpatankar052001@gmail.com>
2024-09-22 19:41:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-console.log("letsssssssssssss writr some javascript");
+console.log("lets writr some javascript");
 let hamburger = document.querySelector(".hamburger")
 let cross = document.querySelector(".cross")
-hamburger.addEventListener("click",function(){
+let god = document.querySelector("god").getElementsByTagName("ul")
+hamburger.addEventListener("click", function () {
     console.log("hamburger cliked")
-    document.querySelector("#left").style.left="0"
+    document.querySelector("#left").style.left = "0"
 })
-cross.addEventListener("click",function(){
+cross.addEventListener("click", function () {
     console.log("cross cliked")
-    document.querySelector("#left").style.left="-120%"
+    document.querySelector("#left").style.left = "-120%"
+})
+cross.addEventListener("click", function () {
+    console.log("god card cliked")
+    document.querySelector(".songlist").innerHTML = `
+    <li>
+        <img class="invert" src="https://ganesh-patankar.github.io/Spotify/img/music.svg" alt="" width="100px">
+                            <div class="info">
+                                <div class="songname">mahadev mahadev </div>  
+                            </div>
+                            <div class="playnow">
+                            <img class="invert" src="https://ganesh-patankar.github.io/Spotify/img/play.svg" alt="" width="35px">
+                            </div>
+        </li>`
 })
