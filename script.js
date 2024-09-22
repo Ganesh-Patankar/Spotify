cb09c2e99064ff0964211220610a985c467700b4
Ganesh-Patankar <ganeshpatankar052001@gmail.com>
2024-09-22 19:11:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 console.log("lets writr some javascript");
-
+let hamburger = document.querySelector(".hamburger")
+hamburger.addEventListener("click",function(){
+    console.log("hamburger cliked")
+    document.querySelector("#left").style.left="0"
+})
