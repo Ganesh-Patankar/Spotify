20105c952e502a5b363e01ce677ad5cc802bd202
Ganesh-Patankar <ganeshpatankar052001@gmail.com>
2024-09-28 10:30:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/files/opening&closing/style.css">
+</head>
+
+<body>
+    
+
+        <div class="massage">
+            <i>
+                <p id="msg"></p>
+            </i>
+            <p id="submsg">Enjoy your favorait music without any iteraption</p>
+
+        </div>
+        <div class="popup">
+            <div class="text">
+                <h1>🙏</h1>
+                <p id="p">Welcome</p>
+                <hr>
+                <hr>
+                <p id="ganesh">This Web-player created by <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ganesh Patankar</p>
+            </div>
+            <div class="username">
+
+                <p> Please Enter Your Name here</p>
+
+            </div>
+
+            <div class="input">
+
+                <input type="text" name="" id="name"><br>
+                <input type="submit" name="" id="button" value="submit"><br>
+
+            </div>
+        </div>
+        <div class="feedback">
+            <div class="rateus">
+                <p>Review Us</p>
+
+            </div>
+            <div class="stars">
+                <img class="star1" src="/files/opening&closing/star.svg" alt="" width="40px">
+                <img class="star2" src="/files/opening&closing/star.svg" alt="" width="40px">
+                <img class="star3" src="/files/opening&closing/star.svg" alt="" width="40px">
+                <img class="star4" src="/files/opening&closing/star.svg" alt="" width="40px">
+                <img class="star5" src="/files/opening&closing/star.svg" alt="" width="40px">
+            </div>
+            <div class="description">
+                <form action="" method="post">
+                    <textarea name="Feedback-description" id="description"
+                        placeholder="Describe your Expirence"></textarea>
+                    <input type="text" name="username" id="getname" value="">
+                    <input type="text" name="rating" id="getrating" value="">
+                    <input type="submit" name="" id="desbutton" value="submit">
+                </form>
+            </div>
+        </div>
+      
+        <script src="/files/opening&closing/script.js">
+
+        </script>
+    
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,119 @@
+console.log("lets write some javascript")
+
+//popup load
+
+window.onload = function() {
+    
+    document.querySelector(".popup").style.display = "block"
+    document.querySelector(".massage").style.display = "none"
+}
+//gettin name of user and end of popupscreen
+let username = document.querySelector("#name");
+
+
+document.querySelector("#button").addEventListener("click", function() {
+    document.querySelector(".popup").style.display = "none"
+    document.querySelector(".massage").style.display = "block"
+    let namevalue = username.value;
+    console.log(namevalue)
+    document.querySelector("#getname").value = `${namevalue}`
+    //greeting screen of user
+    if (namevalue == ""){
+    
+document.querySelector("#msg").innerHTML = `&nbsp;&nbsp;&nbsp;"Hii <br> &nbsp;&nbsp;User"`
+}
+else if (namevalue == "suraj"||"Suraj"|| "SURAJ"|| "Suraj Chavan"|| "Suraj chavan"){
+    document.querySelector("#msg").innerHTML = `&nbsp;&nbsp;&nbsp;"Hii <br> &nbsp;&nbsp;<i>SQ RQ ZQ</i>"` 
+let songsuraj = document.querySelector("#suraj")
+    songsuraj.play();
+}
+else {
+    document.querySelector("#msg").innerHTML = `&nbsp;&nbsp;&nbsp;"Hii <br> &nbsp;&nbsp;${namevalue}"`
+}
+
+//greeting screen off
+setTimeout(function(){
+    document.querySelector(".massage").style.display = "none";
+}, 10000);
+
+
+
+
+//popup rating screen
+setTimeout(function(){
+    document.querySelector(".feedback").style.display = "block";
+}, 60000);
+
+//reting screen content and open description box
+
+let star1  = document.querySelector(".star1")
+star1.addEventListener("click", function(){
+    star1.src = "filledstar.svg";
+    document.querySelector(".rateus>p").innerHTML = "i just hate it😠";
+    document.querySelector(".description").style.display = "block";
+   document.querySelector("#getrating").value = "1/5"
+   let rate =  document.querySelector("#getrating")
+   let rateval = rate.value;
+console.log(rateval)    
+})
+let star2  = document.querySelector(".star2")
+star2.addEventListener("click", function(){
+    star1.src = "filledstar.svg";
+    star2.src = "filledstar.svg";
+    document.querySelector(".rateus>p").innerHTML = "i dont't like it😩";
+    document.querySelector(".description").style.display = "block";
+    document.querySelector("#getrating").value = "2/5"
+    let rate =  document.querySelector("#getrating")
+    let rateval = rate.value;
+ console.log(rateval)    
+})
+let star3  = document.querySelector(".star3")
+star3.addEventListener("click", function(){
+    star1.src = "filledstar.svg";
+    star2.src = "filledstar.svg";
+    star3.src = "filledstar.svg";
+    document.querySelector(".rateus>p").innerHTML = "it is awsome😄";
+    document.querySelector(".description").style.display = "block";
+    document.querySelector("#getrating").value = "3/5"
+    let rate =  document.querySelector("#getrating")
+    let rateval = rate.value;
+ console.log(rateval)    
+})
+let star4  = document.querySelector(".star4")
+star4.addEventListener("click", function(){
+    star1.src = "filledstar.svg";
+    star2.src = "filledstar.svg";
+    star3.src = "filledstar.svg";
+    star4.src = "filledstar.svg";
+    document.querySelector(".rateus>p").innerHTML = " i ust like it😎";
+    document.querySelector(".description").style.display = "block";
+    document.querySelector("#getrating").value = "4/5"
+    let rate =  document.querySelector("#getrating")
+    let rateval = rate.value;
+ console.log(rateval)    
+})
+let star5  = document.querySelector(".star5")
+star5.addEventListener("click", function(){
+    star1.src = "filledstar.svg";
+    star2.src = "filledstar.svg";
+    star3.src = "filledstar.svg";
+    star4.src = "filledstar.svg";
+    star5.src = "filledstar.svg";
+    document.querySelector(".rateus>p").innerHTML = " i just love it😍";
+    document.querySelector(".description").style.display = "block";
+    document.querySelector("#getrating").value = "5/5"
+    let rate =  document.querySelector("#getrating")
+    let rateval = rate.value;
+ console.log(rateval)    
+})
+
+//off the rating screen
+
+let submit  = document.querySelector("#desbutton")
+submit.addEventListener("click" , function(){
+    document.querySelector(".feedback").style.display = "none";
+    document.querySelector(".welcomeandretting").style.display = "none";
+
+})
+
+})
